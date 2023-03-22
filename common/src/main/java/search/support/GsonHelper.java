a7c470cb001f8b4be5516f0d6cac0cd6bc862931
정주화 <qwe19942@thewc.co.kr>
2023-03-22 21:54:42 +0900

    [제거] 불필요 코드 제거

--- common/src/main/java/search/support/GsonHelper.java
+++ common/src/main/java/search/support/GsonHelper.java
@@ -7,19 +7,6 @@ public class GsonHelper {
 
     private static final Gson GSON = new Gson();
 
-
-    public static String toJson(Object obj) {
-        return GSON.toJson(obj);
-    }
-
-    public static String toJson(Object obj, Type type) {
-        return GSON.toJson(obj, type);
-    }
-
-    public static <T> T fromJson(String json, Class<T> clz) {
-        return GSON.fromJson(json, clz);
-    }
-
     public static <T> T fromJson(String json, Type type) {
         return GSON.fromJson(json, type);
     }
